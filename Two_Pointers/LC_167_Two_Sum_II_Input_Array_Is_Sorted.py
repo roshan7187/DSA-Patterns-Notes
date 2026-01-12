bbd2eb4e9271abb6ad0f82e488cb152948abb60c
RKG <roshangupta7187@gmail.com>
2026-01-13 01:06:15 +0530

    Two pointer: Two SumII

--- Two_Pointers/LC_167_Two_Sum_II_Input_Array_Is_Sorted.py
+++ Two_Pointers/LC_167_Two_Sum_II_Input_Array_Is_Sorted.py
@@ -6,22 +6,43 @@ Pattern: Two Pointers
 Link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 
 🧠 Key Insight:
-- 
+- Sorted array → use Two Pointer (optimal).
+
+- Unsorted array + only YES/NO or values → sort + Two Pointer.
+
+- Unsorted array + return indices → Hashing (One-Pass Hash Table) is optimal.
+
+- Two Pointer on unsorted array requires storing (value, index) pairs and sorting → not optimal.
 
 ⚠️ Edge Cases:
-- 
+- Duplicate elements (e.g., [3,3])
+
+- Negative numbers
+
+- Target using same value twice
+
+- Large inputs (avoid sorting if possible)
+
+- 0-based vs 1-based indexing
 
 ❌ Mistake I Made:
-- 
+- None
 
-🧪 Dry Run:
-Input:
-Output:
-Explanation:
-
-Solved On: 2026-01-13
+Solved On: 2026-01-12
 """
 
 class Solution:
-    def solve(self, ...):
-        pass
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left,right = 0,len(numbers)-1
+
+        while(left<right):
+            
+            curr_sum = numbers[left] + numbers[right]
+            if curr_sum == target :
+                return [left+1,right+1]
+            elif curr_sum < target :
+                left+=1
+            else :
+                right-=1
+
+
