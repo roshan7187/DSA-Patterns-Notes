502cd29edf43bbd9a9cfb7eaddbcefad680f506c
RKG <roshangupta7187@gmail.com>
2026-01-13 00:42:48 +0530

    Two pointer: Two SumII

---
+++ Two_Pointers/LC_167_Two_Sum_II_Input_Array_Is_Sorted.py
@@ -0,0 +1,27 @@
+"""
+Problem: LC 167 Two Sum II Input Array Is Sorted
+Platform: LeetCode
+Difficulty: Easy
+Pattern: Two Pointers
+Link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+🧠 Key Insight:
+- 
+
+⚠️ Edge Cases:
+- 
+
+❌ Mistake I Made:
+- 
+
+🧪 Dry Run:
+Input:
+Output:
+Explanation:
+
+Solved On: 2026-01-13
+"""
+
+class Solution:
+    def solve(self, ...):
+        pass
