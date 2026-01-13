549cf7ff2b799da9b017f7367f7a80c376389de1
RKG <roshangupta7187@gmail.com>
2026-01-13 23:26:13 +0530

    Two pointer : Remove Duplicate

---
+++ Two_Pointers/LC_26_Remove_Duplicates_From_Sorted_Array.py
@@ -0,0 +1,33 @@
+"""
+Problem: LC 26 Remove Duplicates From Sorted Array
+Platform: LeetCode
+Difficulty: Easy
+Pattern: Two Pointers
+Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+🧠 Key Insight:
+- 
+
+⚠️ Edge Cases:
+- 
+
+❌ Mistake I Made:
+- 
+
+🧪 Dry Run:
+Input:
+Output:
+Explanation:
+
+Solved On: 2026-01-13
+"""
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        slow = 0
+
+        for fast in range(len(nums)) :
+            if fast == 0 or nums[fast] != nums[fast-1] :
+                nums[slow] = nums[fast]
+                slow+=1
+        return slow
